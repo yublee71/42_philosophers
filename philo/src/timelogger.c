9203cdc0d18963418c6b9ca4d11d34802b3dd501
Yubeen Lee <lybin71@naver.com>
2024-12-22 01:18:02 +0100

    fix: add usleep in timelogger to work with valgrind

--- philo/src/timelogger.c
+++ philo/src/timelogger.c
@@ -6,7 +6,7 @@
 /*   By: yublee <yublee@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 17:24:23 by yublee            #+#    #+#             */
-/*   Updated: 2024/12/21 23:13:49 by yublee           ###   ########.fr       */
+/*   Updated: 2024/12/22 01:12:38 by yublee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,12 @@ void	*timelogger(void *arg)
 	t_table	*table;
 	t_philo	*philo;
 	int		i;
-	int		n;
 
 	table = (t_table *)arg;
-	n = table->info.n_of_philos;
 	while (is_table_active(table))
 	{
 		i = 0;
-		while (i < n)
+		while (i < table->info.n_of_philos)
 		{
 			philo = table->philos[i++];
 			pthread_mutex_lock(&table->death_mutex);
@@ -37,6 +35,7 @@ void	*timelogger(void *arg)
 			}
 			pthread_mutex_unlock(&table->death_mutex);
 		}
+		usleep(100);
 	}
 	return (NULL);
 }
